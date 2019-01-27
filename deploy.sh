8d9294a5e911006b43645a35510304f2cdde9246
Rob Donnelly <rfdonnelly@gmail.com>
2019-01-27 14:40:23 -0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+rm -rf ./build
 npm run build
 cd ./build
+git init
 git add .
 git commit -m 'push to gh-pages'
-git push --force git@github.com:anvaka/pm.git master:gh-pages
+git push --force git@github.com:rfdonnelly/crate-galaxy.git master:gh-pages
